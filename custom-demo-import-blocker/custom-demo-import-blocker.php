cca310a199185fa150374017784322078dbda1d9
Dmitrii Chempalov <120078487+childtheme@users.noreply.github.com>
2024-09-06 10:43:13 +0800

    Update custom-demo-import-blocker.php

--- custom-demo-import-blocker/custom-demo-import-blocker.php
+++ custom-demo-import-blocker/custom-demo-import-blocker.php
@@ -7,146 +7,110 @@ Author: Dmitrii Chemopalov
 */
 
 // Create admin menu
+
 function cdi_add_admin_menu() {
     add_menu_page(
         'Wireframe Import Blocker Settings', 
         'Wireframe Import Blocker', 
         'manage_options', 
-        'cdi_settings', 
-        'cdi_settings_page'
+        'demo-import-blocker', 
+        'cdi_render_settings_page',
+        'dashicons-admin-generic'
     );
 }
-add_action( 'admin_menu', 'cdi_add_admin_menu' );
+add_action('admin_menu', 'cdi_add_admin_menu');
 
-// Settings page content
-function cdi_settings_page() {
+// Register settings
+function cdi_register_settings() {
+    register_setting('cdi_settings_group', 'block_media_placeholders');
+    register_setting('cdi_settings_group', 'block_team_placeholders');
+    register_setting('cdi_settings_group', 'block_quote_placeholders');
+    register_setting('cdi_settings_group', 'block_logo_placeholders');
+    register_setting('cdi_settings_group', 'block_forms');
+}
+add_action('admin_init', 'cdi_register_settings');
+
+// Render the settings page
+function cdi_render_settings_page() {
     ?>
     <div class="wrap">
         <h1>Wireframe Import Blocker Settings</h1>
+        <?php if (isset($_GET['settings-updated']) && $_GET['settings-updated']) { ?>
+            <div id="setting-error-settings_updated" class="updated notice notice-success settings-error is-dismissible">
+                <p><strong>All changes were made successfully.</strong></p>
+            </div>
+        <?php } ?>
         <form method="post" action="options.php">
-            <?php
-            settings_fields( 'cdi_settings_group' );
-            do_settings_sections( 'cdi_settings' );
-            submit_button();
-            ?>
+            <?php settings_fields('cdi_settings_group'); ?>
+            <?php do_settings_sections('cdi_settings_group'); ?>
+            
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">Block Media Placeholders</th>
+                    <td><input type="checkbox" name="block_media_placeholders" value="1" <?php checked(1, get_option('block_media_placeholders'), true); ?> /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Block Team Placeholders</th>
+                    <td><input type="checkbox" name="block_team_placeholders" value="1" <?php checked(1, get_option('block_team_placeholders'), true); ?> /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Block Quote Placeholders</th>
+                    <td><input type="checkbox" name="block_quote_placeholders" value="1" <?php checked(1, get_option('block_quote_placeholders'), true); ?> /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Block Logo Placeholders</th>
+                    <td><input type="checkbox" name="block_logo_placeholders" value="1" <?php checked(1, get_option('block_logo_placeholders'), true); ?> /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Block Wireframe Contact Forms</th>
+                    <td><input type="checkbox" name="block_forms" value="1" <?php checked(1, get_option('block_forms'), true); ?> /></td>
+                </tr>
+            </table>
+            
+            <?php submit_button(); ?>
         </form>
     </div>
     <?php
 }
 
-// Register settings
-function cdi_register_settings() {
-    register_setting( 'cdi_settings_group', 'block_generic_placeholder' );
-    register_setting( 'cdi_settings_group', 'block_team_placeholder' );
-    register_setting( 'cdi_settings_group', 'block_quote_placeholder' );
-    register_setting( 'cdi_settings_group', 'block_logo_placeholder' );
-    register_setting( 'cdi_settings_group', 'block_default_placeholders' );
-    register_setting( 'cdi_settings_group', 'block_contact_forms' );
-
-    add_settings_section( 'cdi_main_section', 'Select Elements to Block', null, 'cdi_settings' );
-
-    add_settings_field(
-        'block_generic_placeholder', 
-        'Block Generic Media Placeholder', 
-        'cdi_checkbox_callback', 
-        'cdi_settings', 
-        'cdi_main_section', 
-        array( 'name' => 'block_generic_placeholder' )
-    );
-    add_settings_field(
-        'block_team_placeholder', 
-        'Block Team Placeholder', 
-        'cdi_checkbox_callback', 
-        'cdi_settings', 
-        'cdi_main_section', 
-        array( 'name' => 'block_team_placeholder' )
-    );
-    add_settings_field(
-        'block_quote_placeholder', 
-        'Block Quote Placeholder', 
-        'cdi_checkbox_callback', 
-        'cdi_settings', 
-        'cdi_main_section', 
-        array( 'name' => 'block_quote_placeholder' )
-    );
-    add_settings_field(
-        'block_logo_placeholder', 
-        'Block Logo Placeholder', 
-        'cdi_checkbox_callback', 
-        'cdi_settings', 
-        'cdi_main_section', 
-        array( 'name' => 'block_logo_placeholder' )
-    );
-    add_settings_field(
-        'block_default_placeholders', 
-        'Block Default Media Placeholders', 
-        'cdi_checkbox_callback', 
-        'cdi_settings', 
-        'cdi_main_section', 
-        array( 'name' => 'block_default_placeholders' )
-    );
-    add_settings_field(
-        'block_contact_forms', 
-        'Block Wireframe Contact Forms', 
-        'cdi_checkbox_callback', 
-        'cdi_settings', 
-        'cdi_main_section', 
-        array( 'name' => 'block_contact_forms' )
-    );
-}
-add_action( 'admin_init', 'cdi_register_settings' );
-
-// Callback for checkboxes
-function cdi_checkbox_callback( $args ) {
-    $option = get_option( $args['name'] );
-    echo '<input type="checkbox" name="' . $args['name'] . '" value="1" ' . checked( 1, $option, false ) . '>';
-}
-
-// Add the filters based on settings
+// Apply the block settings via filters
 function cdi_apply_filters() {
-    if ( get_option( 'block_generic_placeholder' ) ) {
-        add_filter( 'uncode_wireframes_get_generic_placeholder_media_id', 'uncode_custom_generic_placeholder_media_id' );
+    if (get_option('block_media_placeholders')) {
+        add_filter('uncode_wireframes_get_generic_placeholder_media_id', 'cdi_block_generic_placeholder');
     }
-    if ( get_option( 'block_team_placeholder' ) ) {
-        add_filter( 'uncode_wireframes_get_team_placeholder_media_id', 'uncode_custom_team_placeholder_media_id' );
+
+    if (get_option('block_team_placeholders')) {
+        add_filter('uncode_wireframes_get_team_placeholder_media_id', 'cdi_block_team_placeholder');
     }
-    if ( get_option( 'block_quote_placeholder' ) ) {
-        add_filter( 'uncode_wireframes_get_quote_placeholder_media_id', 'uncode_custom_quote_placeholder_media_id' );
+
+    if (get_option('block_quote_placeholders')) {
+        add_filter('uncode_wireframes_get_quote_placeholder_media_id', 'cdi_block_quote_placeholder');
     }
-    if ( get_option( 'block_logo_placeholder' ) ) {
-        add_filter( 'uncode_wireframes_get_logo_placeholder_media_id', 'uncode_custom_logo_placeholder_media_id' );
+
+    if (get_option('block_logo_placeholders')) {
+        add_filter('uncode_wireframes_get_logo_placeholder_media_id', 'cdi_block_logo_placeholder');
     }
-    if ( get_option( 'block_default_placeholders' ) ) {
-        add_filter( 'uncode_wireframes_create_placeholders_medias', 'uncode_custom_block_import_default_placeholders_medias' );
-    }
-    if ( get_option( 'block_contact_forms' ) ) {
-        add_filter( 'uncode_wireframes_create_forms', 'uncode_custom_block_import_default_forms' );
+
+    if (get_option('block_forms')) {
+        add_filter('uncode_wireframes_create_forms', '__return_false');
     }
 }
-add_action( 'init', 'cdi_apply_filters' );
+add_action('init', 'cdi_apply_filters');
 
-// Filter functions
-function uncode_custom_generic_placeholder_media_id( $id ) {
-    $placeholders = array( 5065, 5066, 5067 );
-    return $placeholders[ mt_rand( 0, count( $placeholders ) - 1 ) ];
+// Placeholder blocking functions
+function cdi_block_generic_placeholder($id) {
+    $placeholders = array(5065, 5066, 5067);
+    return $placeholders[array_rand($placeholders)];
 }
 
-function uncode_custom_team_placeholder_media_id( $id ) {
-    return 5065;
+function cdi_block_team_placeholder($id) {
+    return 5065; // Replace with desired media ID
 }
 
-function uncode_custom_quote_placeholder_media_id( $id ) {
-    return 5065;
+function cdi_block_quote_placeholder($id) {
+    return 5065; // Replace with desired media ID
 }
 
-function uncode_custom_logo_placeholder_media_id( $id ) {
-    return 5065;
-}
-
-function uncode_custom_block_import_default_placeholders_medias() {
-    return false;
-}
-
-function uncode_custom_block_import_default_forms() {
-    return false;
+function cdi_block_logo_placeholder($id) {
+    return 5065; // Replace with desired media ID
 }
